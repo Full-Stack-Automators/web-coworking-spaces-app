7b1677ab0fde21a97fe8e0c40144c83f941134ab
Ola Akinola <ola@192.168.1.106>
2023-08-30 09:53:01 +0100

    Hide jira keys and print secrets

--- cypress.config.ts
+++ cypress.config.ts
@@ -55,12 +55,9 @@ export default defineConfig({
     specPattern: 'cypress/integration/**',
   },
   env: {
-    // XRAY_CLIENT_ID: process.env.XRAY_CLIENT_ID,
-    XRAY_CLIENT_ID: "402C4276B9614A70BE3D9E24AA1F459F",
-    // XRAY_CLIENT_SECRET: process.env.XRAY_CLIENT_SECRET,
-    XRAY_CLIENT_SECRET: "e4abcdf917230e9d9fc2ec129f0ced46ceb26ae609c59a09e11d2735dc8fdb59",
-    JIRA_USERNAME: 'ola.akinola@fullstackautomators.com',
-    // JIRA_API_TOKEN: process.env.JIRA_API_TOKEN,
-    JIRA_API_TOKEN: "ATATT3xFfGF0LSRrOWQNpel8CrBNwg2a1HHJp0jXVmi09VGfd1TJ69QsTBRTyYni6TQUiu87eg_yC4FZ3PzGIhHZGmNOFLEl7WgPL6J4HJelIHSMNo_t8QMgfvms0xjyRDRvVcX8sMEJpzZicVW44o6LB0C3cNgVfozRAFi8WEPaHNvRwLxTOxA=470E2AF7",
+    XRAY_CLIENT_ID: process.env.XRAY_CLIENT_ID,
+    XRAY_CLIENT_SECRET: process.env.XRAY_CLIENT_SECRET,
+    JIRA_USERNAME: 'admin@fullstackautomators.com',
+    JIRA_API_TOKEN: process.env.JIRA_API_TOKEN,
   }
 });
